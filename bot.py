58d8e8d42c5866927f0e7e2c5b1781e9374f986e
botsitopescao <lavozpiu@outlook.com>
2025-02-11 00:56:02 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,10 +17,10 @@ from zoneinfo import ZoneInfo  # Para trabajar con zonas horarias
 # CONFIGURACIÓN: IDs y Servidor
 ######################################
 OWNER_ID = 1336609089656197171         # Tu Discord ID (único autorizado para comandos sensibles)
-PRIVATE_CHANNEL_ID = 1338130641354620988  # Canal privado para comandos sensibles (no se utiliza en la versión final)
-PUBLIC_CHANNEL_ID  = 1338126297666424874  # Canal público donde se muestran resultados sensibles
-SPECIAL_HELP_CHANNEL = 1337387112403697694  # Canal especial para que el owner reciba la lista extendida de comandos
-GUILD_ID = 123456789012345678            # REEMPLAZA con el ID real de tu servidor (guild)
+#PRIVATE_CHANNEL_ID = 1338130641354620988  # Canal privado para comandos sensibles (no se utiliza en la versión final)
+PUBLIC_CHANNEL_ID  = 1338126297666424874  # Canal público donde se muestran resultados sensibles, este es el canal puntuacion
+SPECIAL_HELP_CHANNEL = 1338747286901100554  # Canal especial para que el owner reciba la lista extendida de comandos, canal lista de comandos
+GUILD_ID = 1337387112403697694            # REEMPLAZA con el ID real de tu servidor (guild)
 
 API_SECRET = os.environ.get("API_SECRET")  # Para la API privada (opcional)
 
